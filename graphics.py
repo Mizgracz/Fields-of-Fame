20b631bf1b2d867935f9c6cac22e49f5e9a85bd4
Lucyferiusz <127144420+Lucyferiusz@users.noreply.github.com>
2023-03-26 16:06:03 +0200

    ojektowość menu budowania
    + drobne poprawki

--- graphics.py
+++ graphics.py
@@ -2,26 +2,23 @@ import pygame
 import random
 import math
 
-class ClassName(object):
-    """docstring for ClassName"""
-    def __init__(self, arg):
-        super(ClassName, self).__init__()
-        self.arg = arg
-        
+
 
 class Map(pygame.sprite.Group):
     def __init__(self, numx, numy,screen):
         super().__init__()
         self.screen = screen
-        self.grass_surface = pygame.image.load("tekstury/trawa_hex.png", ).convert_alpha()
+        self.grass_surface = pygame.image.load("tekstury/grass.png", ).convert_alpha()
         self.num_hex_x = numx
         self.num_hex_y = numy
         self.allhex = {}
         self.alltex = {}
+        self.offset = pygame.Vector2(0,30)
+        self.s = pygame.Surface((self.screen.get_size()[0]-300,self.screen.get_size()[1]))
+        self.s_rect = self.s.get_rect(topleft = (0,30))
+        self.PlayerHex = 0
+        self.BotHex = 0
 
-    def texture(self):
-        for i in range (self.num_hex_y * self.num_hex_x):
-            print()
     def generate(self):
 
         licz = 0
@@ -42,28 +39,29 @@ class Map(pygame.sprite.Group):
             przesuniecie_y += -38  #
 
     def draw(self):  # wyświetlanie mapy na ekranie
-        self.s = pygame.Surface((self.num_hex_x*130.,self.num_hex_y*152))
-        self.s.fill('#ffffff')
+        
+        self.s.fill('#000000')
         for h in self.sprites():
             self.s.blit(h.texture, (h.polozenie_hex_x, h.polozenie_hex_y))
             
             # self.s.blit(h.owner, (h.polozenie_hex_x, h.polozenie_hex_y))
-        self.screen.blit(self.s,(0,0))
+        self.screen.blit(self.s,(0,30))
 class Hex(pygame.sprite.Sprite):
     def __init__(self, x, y, tex, num,group):
         super().__init__(group)
+        
         self.szerokosc = 130
         self.wysokosc = 152
         self.polozenie_hex_x = x
         self.polozenie_hex_y = y
-        self.texture = pygame.image.load("tekstury/trawa_hex.png", ).convert_alpha()
+        self.texture = pygame.image.load("tekstury/grass.png", ).convert_alpha()
         self.verticles = [
-        (self.polozenie_hex_x+self.szerokosc/2,self.polozenie_hex_y),#v1
-        (self.polozenie_hex_x+self.szerokosc,self.polozenie_hex_y+self.wysokosc/4),#v2
-        (self.polozenie_hex_x+self.szerokosc,self.polozenie_hex_y+self.wysokosc-self.wysokosc/4),#v2
-        (self.polozenie_hex_x+self.szerokosc/2,self.polozenie_hex_y+self.wysokosc),#v4
-        (self.polozenie_hex_x,self.polozenie_hex_y+self.wysokosc-self.wysokosc/4),#v2
-        (self.polozenie_hex_x,self.polozenie_hex_y+self.wysokosc/4),#v2
+        (self.polozenie_hex_x+self.szerokosc/2,self.polozenie_hex_y+group.offset.y),#v1
+        (self.polozenie_hex_x+self.szerokosc,self.polozenie_hex_y+self.wysokosc/4+group.offset.y),#v2
+        (self.polozenie_hex_x+self.szerokosc,self.polozenie_hex_y+self.wysokosc-self.wysokosc/4+group.offset.y),#v2
+        (self.polozenie_hex_x+self.szerokosc/2,self.polozenie_hex_y+self.wysokosc+group.offset.y),#v4
+        (self.polozenie_hex_x,self.polozenie_hex_y+self.wysokosc-self.wysokosc/4+group.offset.y),#v2
+        (self.polozenie_hex_x,self.polozenie_hex_y+self.wysokosc/4+group.offset.y),#v2
         ]
         self.verticles_texture = [
         (self.szerokosc/2,0),#v1
@@ -73,14 +71,10 @@ class Hex(pygame.sprite.Sprite):
         (0,self.wysokosc-self.wysokosc/4),#v2
         (0,self.wysokosc/4),#v2
         ]
-        self.rect = self.texture.get_rect(topleft=(self.polozenie_hex_x , self.polozenie_hex_y))
-        self.owner =pygame.Surface((130,152),pygame.SRCALPHA)
-
+        
+        self.owner = pygame.Surface((130,152),pygame.SRCALPHA)
         self.owner_rect = self.owner.get_rect(topleft = (self.polozenie_hex_x , self.polozenie_hex_y) )
-        
-        # self.owner.blit(self.texture,(0,0))
-        
-        self.owner.set_alpha(255*(30/100))
+        self.owner.set_alpha(255*(10/100))
         self.texture.blit(self.owner,(0,0))
 
         self.type = None
@@ -109,18 +103,14 @@ class Hex(pygame.sprite.Sprite):
 
         # Jeśli suma pól trójkątów jest równa polu wielokąta, to punkt znajduje się wewnątrz
         return point_area == polygon_area
-    def function(self,group):
+    def change_type_hex(self,group):
         if self.type !='Player':
-            print('Y:',self.polozenie_hex_y,'X:',self.polozenie_hex_x)
-            group.screen.fill('#ffffff')
             group.draw()
             polygon = pygame.draw.polygon(self.owner, (0,255,0), self.verticles_texture)
-            
             self.texture.blit(self.owner,(0,0))
-            # self.owner.set_alpha(128)
             self.type = 'Player'
             group.s.blit(self.owner,self.owner_rect)
-            group.screen.blit(group.s,(0,0))
+            group.screen.blit(group.s,(0,30))
+            group.PlayerHex +=1
         else:
             print('Posiadasz to pole')
-            # print(group.num_hex_x)
